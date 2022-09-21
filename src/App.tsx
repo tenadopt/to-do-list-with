79773d98d21a7fcb40df7677bdfe1e628853d429
Tenad <46803834+tenadopt@users.noreply.github.com>
2022-09-21 15:29:04 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ToDoList} from "./ToDoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const title1 = 'What to learn'
+    const title2 = 'What to do'
+
+    const task1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+
+    const task2 = [
+        {id: 1, title: 'HTML&CSS', isDone: false},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+
+
+    return (
+
+        <div className="App">
+            <ToDoList title={title1} tasks={task1}/>
+            <ToDoList title={title2} tasks={task2}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/ToDoList.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type ToDoListProps = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+
+export const ToDoList = (props: ToDoListProps) => {
+
+    return (
+
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+
+        </div>
+    );
+}
